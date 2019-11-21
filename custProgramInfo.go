4dcdbb95bdf8048c4fb03f030f9fe7dca6536124
paulil <pauline.li@amd.com>
2019-11-21 13:48:35 +0800

    Manage data in a separate file -customProgramInfo

---
+++ custProgramInfo.go
@@ -0,0 +1,44 @@
+
+package smtauto
+
+import (
+//    "fmt"
+
+)
+
+type AsicConf struct {
+    AsicName string
+    StackName string
+    TargetRelease string
+    VBIOS string
+    OSDB string
+}
+
+type StackConf struct {
+    Version string
+    StackPath string
+    TestReport string
+    LnxStack []AsicConf
+}
+
+var (
+    targetReleaseMap = map[string]string {
+        "D182": "19.40",
+        "D187": "19.40",
+        "D188": "19.50",
+        "D189": "19.50",
+    }
+
+    asicNameMap = map[string]string {
+        "D182": "Navi10 XT",
+        "D187": "Navi10 XM",
+        "D188": "Navi10 Pro-XL",
+        "D189": "Navi10 XLE",
+    }
+
+    stackConf = StackConf{
+        Version: "WW46",
+        StackPath: "/opt/shares/Navi10_Stack/",
+    }
+)
+
