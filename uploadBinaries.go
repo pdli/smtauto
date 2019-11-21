3a0d9865100462cfb6880f55a766f46fab98f674
paulil <pauline.li@amd.com>
2019-11-21 19:14:34 +0800

    add func to check binary status in SMT

--- uploadBinaries.go
+++ uploadBinaries.go
@@ -17,19 +17,9 @@ func uploadBtnLoaded(wd webdriver.WebDriver)(bool, error){
     return true, err
 }
 
-func UploadBinaries(wd webdriver.WebDriver)( webdriver.WebDriver ){
+func binaryNotExisted(wd webdriver.WebDriver)(bool) {
 
-    if err := wd.WaitWithTimeout(uploadBtnLoaded, 10 * time.Second); err != nil {
-        log.Fatal( err )
-    }else {
-        log.Println("Will upload binaries")
-    }
-
-    uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'UPLOAD BINARIES')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    uploadBtn.Click()
+    notFound := true
 
     versionQuery, err := wd.FindElement(webdriver.ByID, "mat-input-1")
     if err != nil {
@@ -48,7 +38,41 @@ func UploadBinaries(wd webdriver.WebDriver)( webdriver.WebDriver ){
         log.Fatal( err )
     }else {
         log.Println("Catch up you")
+        notFound = false
     }
+
+    return notFound
+}
+
+func getNewBinToUpload(wd webdriver.WebDriver) (){
+    
+    log.Println(stackConf.LnxStack)
+
+//    return stackConf.AsicConf
+}
+
+func UploadBinaries(wd webdriver.WebDriver)( webdriver.WebDriver ){
+
+    if err := wd.WaitWithTimeout(uploadBtnLoaded, 10 * time.Second); err != nil {
+        log.Fatal( err )
+    }else {
+        log.Println("Will upload binaries")
+    }
+
+    uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'UPLOAD BINARIES')]")
+    if err != nil {
+        log.Fatal( err )
+    }
+    uploadBtn.Click()
+
+    if notFound := binaryNotExisted(wd); notFound == true {
+
+        log.Println("Not found Element")
+    } else {
+        log.Println("Found Element")
+    }
+
+    getNewBinToUpload(wd)
 
     return wd
 
