4c85a4a8105b1c61c7096dde50c793edb1153207
paulil <pauline.li@amd.com>
2019-11-21 17:32:29 +0800

    add scripts to manipulate webpage

---
+++ logonSMT.go
@@ -0,0 +1,66 @@
+package smtauto
+
+import (
+//    "fmt"
+    "flag"
+    "log"
+    "time"
+    "github.com/radutopala/webdriver"
+    "github.com/radutopala/webdriver/chrome"
+)
+
+func newChromeDriver() (webdriver.WebDriver ) {
+
+    chromeBinary := flag.String("chrome_binary", "/usr/bin/chromium-browser", "The name of the Chrome binary or the path to it. If name is not an exact path, the PATH will be searched.")
+    chrCaps := chrome.Capabilities{
+        Path: *chromeBinary,
+        Args: []string{
+            //"--headless",
+            //"--disable-gpu",
+            //"--no-sandbox",
+            //"--window-size=800,600",
+        },
+    }
+    caps := webdriver.Capabilities{
+        "browserName": "chrome",
+        "path": chromeBinary,
+    }
+    caps.AddChrome( chrCaps )
+
+    wd, err := webdriver.NewRemote(caps, "")
+    if err != nil {
+        log.Fatal( err )
+    }
+    //defer wd.Quit()
+
+    if err := wd.SetImplicitWaitTimeout( 30 * time.Second ); err != nil {
+        log.Fatal( err )
+    }
+
+    return wd
+}
+
+func mainPageLoaded(wd webdriver.WebDriver) (bool, error){
+
+    _, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'VIEW STACKS')]")
+    if err != nil{
+        return false, err
+    }
+
+    return true, nil
+}
+
+func LogonSMT( smtUrl string) (){
+
+    wd := newChromeDriver()
+
+    if err := wd.Get( smtUrl ); err != nil {
+        log.Fatal( err )
+    }
+
+    if err := wd.WaitWithTimeout(mainPageLoaded, 90 * time.Second); err != nil {
+        log.Fatal( err )
+    } else {
+        log.Println("Log in successfully")
+    } 
+}
