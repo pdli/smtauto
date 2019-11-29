1a5aa735a86caba75c4ba8fbe11771d629e21027
paulil <pauline.li@amd.com>
2019-11-29 15:49:17 +0800

    fix bug to update stack

--- updateNavi10SMT.go
+++ updateNavi10SMT.go
@@ -76,7 +76,7 @@ func uploadVbiosBinary(wd webdriver.WebDriver, biosVersion string) (error){
     versionSearchInput.Clear()
     versionSearchInput.SendKeys( biosVersion )
 
-    searchBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@role='dialog']//*[contains(text(), 'search')]")
+    searchBtn, err := wd.FindElement(webdriver.ByXPATH, "//div[@class='cdk-overlay-pane']//*[contains(text(), 'search')]")
     if err != nil {
         log.Fatal( err )
     }
