ec08f12c03781271a0f4f129fb5ef477dd467ef0
Paulil <pauline.li@amd.com>
2019-12-04 17:34:55 +0800

    Update chrome on Windwos in headless mode

--- logonSMT.go
+++ logonSMT.go
@@ -1,86 +1,89 @@
 package smtauto
 
 import (
-    "os"
-    "flag"
-    "log"
-    "time"
-    "github.com/radutopala/webdriver"
-    "github.com/radutopala/webdriver/chrome"
+	"log"
+	"os"
+	"time"
+
+	"github.com/radutopala/webdriver"
+	"github.com/radutopala/webdriver/chrome"
 )
 
-func newChromeDriver() (webdriver.WebDriver ) {
+func newChromeDriver() webdriver.WebDriver {
 
-    homeDir := os.Getenv("HOME")
+	homeDir := os.Getenv("HOME")
 
-    chromeBinary := flag.String("chrome_binary", "/usr/bin/chromium-browser", "The name of the Chrome binary or the path to it. If name is not an exact path, the PATH will be searched.")
-    chrCaps := chrome.Capabilities{
-        Path: *chromeBinary,
-        Args: []string{
-            //"--headless",
-            //"--disable-gpu",
-            //"--no-sandbox",
-            //"--window-size=800,600",
-            "--user-data-dir="+ homeDir +"/.config/chromium/",
-        },
-    }
-    caps := webdriver.Capabilities{
-        "browserName": "chrome",
-        "path": chromeBinary,
-    }
-    caps.AddChrome( chrCaps )
+	//    chromeBinary := flag.String("chrome_binary", "/usr/bin/chromium-browser", "The name of the Chrome binary or the path to it. If name is not an exact path, the PATH will be searched.")
+	//	chromeBinary := flag.String("chrome_binary", "C:/Program Files (x86)/Google/Chrome/Application", "")
+	chrCaps := chrome.Capabilities{
+		//Path: *chromeBinary,
+		Args: []string{
+			"--headless",
+			"--disable-gpu",
+			"--no-sandbox",
+			"--log-level=1",
+			//"--window-size=800,600",
+			"--remote-debugging-port=9222",
+			"--disable-dev-shm-usage",
+			"--user-data-dir=" + homeDir + "/.config/chromium/",
+		},
+	}
+	caps := webdriver.Capabilities{
+		"browserName": "chrome",
+		//		"path":        chromeBinary,
+	}
+	caps.AddChrome(chrCaps)
 
-    wd, err := webdriver.NewRemote(caps, "")
-    if err != nil {
-        log.Fatal( err )
-    }
-    //defer wd.Quit()
+	wd, err := webdriver.NewRemote(caps, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer wd.Quit()
 
-    if err := wd.SetImplicitWaitTimeout( 30 * time.Second ); err != nil {
-        log.Fatal( err )
-    }
+	if err := wd.SetImplicitWaitTimeout(30 * time.Second); err != nil {
+		log.Fatal(err)
+	}
 
-    return wd
+	return wd
 }
 
-func mainPageLoaded(wd webdriver.WebDriver) (bool, error){
+func mainPageLoaded(wd webdriver.WebDriver) (bool, error) {
 
-    _, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'ACTIVE PROGRAMS LIST')]")
-    if err != nil{
-        return false, err
-    }
+	_, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'ACTIVE PROGRAMS LIST')]")
+	if err != nil {
+		return false, err
+	}
 
-    return true, nil
+	return true, nil
 }
 
-func NewWebService() ( *webdriver.Service){
+func NewWebService() *webdriver.Service {
 
-    service, err := webdriver.NewService()
-    if err != nil {
-        log.Fatal( err )
-    }
+	service, err := webdriver.NewService()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return service
+	return service
 }
 
-func LogonSMT( smtUrl string) (wd webdriver.WebDriver){
+func LogonSMT(smtUrl string) (wd webdriver.WebDriver) {
 
-    wd = newChromeDriver()
+	wd = newChromeDriver()
 
-    if err := wd.Get( smtUrl ); err != nil {
-        log.Fatal( err )
-    }
+	if err := wd.Get(smtUrl); err != nil {
+		log.Fatal(err)
+	}
 
-    time.Sleep( 5 * time.Second )
+	time.Sleep(5 * time.Second)
 
-    //TODO: automatically login SMT
+	//TODO: automatically login SMT
 
-    if err := wd.WaitWithTimeout(mainPageLoaded, 90 * time.Second); err != nil {
-        log.Fatal( err )
-    } else {
-        log.Println("Log in successfully")
-    }
+	if err := wd.WaitWithTimeout(mainPageLoaded, 90*time.Second); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Log in successfully")
+	}
 
-    return wd
+	return wd
 }
-
--- variables.go
+++ variables.go
@@ -1,62 +1,57 @@
-
 package smtauto
 
-import (
 //    "fmt"
 
-)
-
 type AsicConf struct {
-    AsicName string
-    StackName string
-    TargetRelease string
-    VbiosVersion string
-    VbiosFileName string
-    OsdbVersion string
-    OsdbID string
+	AsicName      string
+	StackName     string
+	TargetRelease string
+	VbiosVersion  string
+	VbiosFileName string
+	OsdbVersion   string
+	OsdbID        string
 }
 
 type StackConf struct {
-    Version string
-    StackPath string
-    TestReport string
-    LnxStack []AsicConf
+	Version    string
+	StackPath  string
+	TestReport string
+	LnxStack   []AsicConf
 }
 
 type VbiosConf struct {
-    Name string
-    Link string
-    Version string
-    LastVersion string
+	Name        string
+	Link        string
+	Version     string
+	LastVersion string
 }
 
 var (
-    targetReleaseMap = map[string]string {
-        "D182": "19.40",
-        "D187": "19.40",
-        "D188": "19.50",
-        "D189": "19.50",
-    }
+	targetReleaseMap = map[string]string{
+		"D182": "19.40",
+		"D187": "19.40",
+		"D188": "19.50",
+		"D189": "19.50",
+	}
 
-    asicNameMap = map[string]string {
-        "D182": "Navi10 XT",
-        "D187": "Navi10 XM",
-        "D188": "Navi10 Pro-XL",
-        "D189": "Navi10 XLE",
-    }
+	asicNameMap = map[string]string{
+		"D182": "Navi10 XT",
+		"D187": "Navi10 XM",
+		"D188": "Navi10 Pro-XL",
+		"D189": "Navi10 XLE",
+	}
 
-    vbiosFileNameList = []string{
-        "D1820101",
-        "D1870101",
-        "D1880201",
-        "D1890101",
-    }
+	vbiosFileNameList = []string{
+		"D1820101",
+		"D1870101",
+		"D1880201",
+		"D1890101",
+	}
 
-    stackConf = StackConf{
-        Version: "WW47",
-        StackPath: "/opt/shares/Navi10_Stack/",
-    }
+	stackConf = StackConf{
+		Version:   "WW47",
+		StackPath: "//pauline.amd.com/shares/Navi10_Stack/",
+	}
 
-    vbiosConf = []VbiosConf{}
+	vbiosConf = []VbiosConf{}
 )
-
