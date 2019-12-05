38b6feb1dc145c1babe099b77a77e9c2824e0a00
Paulil <pauline.li@amd.com>
2019-12-05 10:42:53 +0800

    Update log

--- uploadBinaries.go
+++ uploadBinaries.go
@@ -1,277 +1,276 @@
 package smtauto
 
-import(
+import (
+	"log"
+	"time"
 
-  "log"
-  "time"
-  "github.com/radutopala/webdriver"
+	"github.com/radutopala/webdriver"
 )
 
-func uploadBtnLoaded(wd webdriver.WebDriver)(bool, error){
+func uploadBtnLoaded(wd webdriver.WebDriver) (bool, error) {
 
-    _, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'UPLOAD BINARIES')]");
-    if err != nil {
-        return false, err
-    }
+	_, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'UPLOAD BINARIES')]")
+	if err != nil {
+		return false, err
+	}
 
-    return true, err
+	return true, err
 }
 
-func binNotExisted(wd webdriver.WebDriver, binVersion string)(bool) {
+func binNotExisted(wd webdriver.WebDriver, binVersion string) bool {
 
-    notFound := true
+	notFound := true
 
-    if err := wd.Get("http://smt.amd.com/#/upload?uploadID="); err != nil {
-        log.Fatal( err )
-    }
+	if err := wd.Get("http://smt.amd.com/#/upload?uploadID="); err != nil {
+		log.Fatal(err)
+	}
 
-    versionQuery, err := wd.FindElement(webdriver.ByID, "mat-input-1")
-    if err != nil {
-        log.Fatal( err )
-    }
-    versionQuery.Clear()
-    versionQuery.SendKeys( binVersion )
+	versionQuery, err := wd.FindElement(webdriver.ByID, "mat-input-1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	versionQuery.Clear()
+	versionQuery.SendKeys(binVersion)
 
-    searchMatIcon, err := wd.FindElement(webdriver.ByXPATH, "//div[@class='mat-form-field-suffix ng-tns-c11-3 ng-star-inserted']/*[contains(text(), 'search')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    searchMatIcon.Click()
+	searchMatIcon, err := wd.FindElement(webdriver.ByXPATH, "//div[@class='mat-form-field-suffix ng-tns-c11-3 ng-star-inserted']/*[contains(text(), 'search')]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	searchMatIcon.Click()
 
-    _, err = wd.FindElement(webdriver.ByXPATH, "//div[@class='query-results ng-star-inserted']/*[contains(text(), '" + binVersion + "')]")
-    if err != nil {
-        log.Println( "Not found binary ==> ", binVersion )
-    }else {
-        log.Println("Catch up you")
-        notFound = false
-    }
+	_, err = wd.FindElement(webdriver.ByXPATH, "//div[@class='query-results ng-star-inserted']/*[contains(text(), '"+binVersion+"')]")
+	if err != nil {
+		log.Println("Not found binary ==> ", binVersion)
+	} else {
+		log.Println("Catch up you ==> ", binVersion)
+		notFound = false
+	}
 
-    wd.Refresh()
+	wd.Refresh()
 
-    return notFound
+	return notFound
 }
 
 func unique(strSlice []string) []string {
-    keys := make(map[string]bool)
-    list := []string{}
-    for _, entry := range strSlice {
-        if _, value := keys[entry]; !value {
-            keys[entry] = true
-            list = append(list, entry)
-        }
-    }
-    return list
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range strSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
 }
 
-func getNewBinToUpload(wd webdriver.WebDriver) ([]string){
+func getNewBinToUpload(wd webdriver.WebDriver) []string {
 
-    log.Println(stackConf.LnxStack)
+	log.Println(stackConf.LnxStack)
 
-    binSlice := make([]string, 2*len(stackConf.LnxStack))
+	binSlice := make([]string, 2*len(stackConf.LnxStack))
 
-    count :=0
+	count := 0
 
-    //Get new VBIOS
-    for  index,_ := range stackConf.LnxStack {
+	//Get new VBIOS
+	for index, _ := range stackConf.LnxStack {
 
-        if notFound := binNotExisted(wd, stackConf.LnxStack[ index].VbiosVersion); notFound == true {
-            binSlice[count] = stackConf.LnxStack[index].VbiosVersion
-            count ++
-        }
-    }
+		if notFound := binNotExisted(wd, stackConf.LnxStack[index].VbiosVersion); notFound == true {
+			binSlice[count] = stackConf.LnxStack[index].VbiosVersion
+			count++
+		}
+	}
 
+	//get new OSDB
+	for index, _ := range stackConf.LnxStack {
 
-    //get new OSDB
-    for index, _ := range stackConf.LnxStack {
+		if notFound := binNotExisted(wd, stackConf.LnxStack[index].OsdbVersion); notFound == true {
+			binSlice[count] = stackConf.LnxStack[index].OsdbVersion
+			count++
+		}
+	}
 
-        if notFound := binNotExisted(wd, stackConf.LnxStack[index].OsdbVersion); notFound == true {
-            binSlice[count] = stackConf.LnxStack[index].OsdbVersion
-            count ++
-        }
-    }
+	//unique binary slice
 
-    //unique binary slice
+	binSlice = append(binSlice[:count])
+	binSlice = unique(binSlice)
 
-    binSlice = append(binSlice[:count])
-    binSlice = unique(binSlice)
+	log.Println(binSlice)
 
-    log.Println(binSlice)
-
-    return binSlice
-//    return stackConf.AsicConf
+	return binSlice
+	//    return stackConf.AsicConf
 }
 
 func uploadOSDB(wd webdriver.WebDriver, asicConf AsicConf) {
 
-    log.Println("==> To upload OSDB, ", asicConf)
+	log.Println("==> To upload OSDB, ", asicConf)
 
-    if err := wd.Get("http://smt.amd.com/#/upload?uploadID="); err != nil {
-        log.Fatal( err )
-    }
+	if err := wd.Get("http://smt.amd.com/#/upload?uploadID="); err != nil {
+		log.Fatal(err)
+	}
 
-    //refresh webpage for loop
-    wd.Refresh()
+	//refresh webpage for loop
+	wd.Refresh()
 
-    osdbRadioBtn, err := wd.FindElement(webdriver.ByID, "mat-radio-2")
-    if err != nil {
-        log.Fatal( err )
-    }
-    osdbRadioBtn.Click()
+	osdbRadioBtn, err := wd.FindElement(webdriver.ByID, "mat-radio-2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	osdbRadioBtn.Click()
 
-    swNameInput, err := wd.FindElement(webdriver.ByID, "sw")
-    if err != nil {
-        log.Fatal( err )
-    }
-    swNameInput.Clear()
-    swNameInput.SendKeys("AMD GPU DRIVE")
+	swNameInput, err := wd.FindElement(webdriver.ByID, "sw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	swNameInput.Clear()
+	swNameInput.SendKeys("AMD GPU DRIVE")
 
-    swListBox, err := wd.FindElement(webdriver.ByXPATH, "//div[@id='mat-autocomplete-0']//span[contains(text(), 'AMD GPU DRIVER')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    swListBox.Click()
+	swListBox, err := wd.FindElement(webdriver.ByXPATH, "//div[@id='mat-autocomplete-0']//span[contains(text(), 'AMD GPU DRIVER')]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	swListBox.Click()
 
-    httpLinkInput, err := wd.FindElement(webdriver.ByID, "link")
-    if err != nil {
-        log.Fatal( err )
-    }
-    httpLinkInput.Clear()
-    //http://lnx-jfrog/artifactory/osibuild-packages-cache/949708/hybrid_rel.u1804_64/amdgpu-pro-19.50-949708-ubuntu-18.04.tar.xz
-    ubuntuLink := "http://lnx-jfrog/artifactory/osibuild-packages-cache/" +
-                  asicConf.OsdbID +
-                  "/hybrid_rel.u1804_64/amdgpu-pro-" +
-                  asicConf.OsdbVersion +
-                  "-ubuntu-18.04.tar.xz"
-    httpLinkInput.SendKeys( ubuntuLink )
+	httpLinkInput, err := wd.FindElement(webdriver.ByID, "link")
+	if err != nil {
+		log.Fatal(err)
+	}
+	httpLinkInput.Clear()
+	//http://lnx-jfrog/artifactory/osibuild-packages-cache/949708/hybrid_rel.u1804_64/amdgpu-pro-19.50-949708-ubuntu-18.04.tar.xz
+	ubuntuLink := "http://lnx-jfrog/artifactory/osibuild-packages-cache/" +
+		asicConf.OsdbID +
+		"/hybrid_rel.u1804_64/amdgpu-pro-" +
+		asicConf.OsdbVersion +
+		"-ubuntu-18.04.tar.xz"
+	httpLinkInput.SendKeys(ubuntuLink)
 
-    versionInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='alias']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    versionInput.Clear()
-    versionInput.SendKeys( asicConf.OsdbVersion )
+	versionInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='alias']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	versionInput.Clear()
+	versionInput.SendKeys(asicConf.OsdbVersion)
 
-    osInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='os']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    osInput.Clear()
-    osInput.SendKeys("Linux Ubuntu 18.04 LTS")
+	osInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='os']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	osInput.Clear()
+	osInput.SendKeys("Linux Ubuntu 18.04 LTS")
 
-    osListBox, err := wd.FindElement(webdriver.ByXPATH, "//div[@id='mat-autocomplete-1']//span[contains(text(), 'Linux Ubuntu 18.04')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    osListBox.Click()
+	osListBox, err := wd.FindElement(webdriver.ByXPATH, "//div[@id='mat-autocomplete-1']//span[contains(text(), 'Linux Ubuntu 18.04')]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	osListBox.Click()
 
-    uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@class='submit-button mat-raised-button']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    uploadBtn.Click()
+	uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@class='submit-button mat-raised-button']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	uploadBtn.Click()
 
 }
 
 func uploadBIOS(wd webdriver.WebDriver, asicConf AsicConf) {
 
-    log.Println("==> To upload VBIOS, ", asicConf)
+	log.Println("==> To upload VBIOS, ", asicConf)
 
-    if err := wd.Get("http://smt.amd.com/#/upload?uploadID="); err != nil {
-        log.Fatal( err )
-    }
+	if err := wd.Get("http://smt.amd.com/#/upload?uploadID="); err != nil {
+		log.Fatal(err)
+	}
 
-    //refresh webpage for loop
-    wd.Refresh()
+	//refresh webpage for loop
+	wd.Refresh()
 
-    biosRadioBtn, err := wd.FindElement(webdriver.ByID, "mat-radio-3")
-    if err != nil {
-        log.Fatal( err )
-    }
-    biosRadioBtn.Click()
+	biosRadioBtn, err := wd.FindElement(webdriver.ByID, "mat-radio-3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	biosRadioBtn.Click()
 
-    programInput, err := wd.FindElement(webdriver.ByXPATH, "//div[@class='mat-form-field-infix']/input[@id='cont']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    programInput.Clear()
-    programInput.SendKeys("Navi 10")
+	programInput, err := wd.FindElement(webdriver.ByXPATH, "//div[@class='mat-form-field-infix']/input[@id='cont']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	programInput.Clear()
+	programInput.SendKeys("Navi 10")
 
-    fileUploadTab, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'File Upload')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    fileUploadTab.Click()
+	fileUploadTab, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'File Upload')]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileUploadTab.Click()
 
-//    clickHereBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='mat-tab-content-1-3']/div/div/div/button")
-//    if err != nil {
-//        log.Fatal( err )
-//    }
-//    clickHereBtn.Click()
+	//    clickHereBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='mat-tab-content-1-3']/div/div/div/button")
+	//    if err != nil {
+	//        log.Fatal( err )
+	//    }
+	//    clickHereBtn.Click()
 
-    fileInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='mat-tab-content-1-3']/div/div/div/input")
-    if err != nil {
-        log.Fatal( err )
-    }
-    fileInput.Clear()
-    fileInput.SendKeys(stackConf.StackPath + "/" + stackConf.Version + "/" + asicConf.VbiosFileName )
+	fileInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='mat-tab-content-1-3']/div/div/div/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileInput.Clear()
+	fileInput.SendKeys(stackConf.StackPath + "/" + stackConf.Version + "/" + asicConf.VbiosFileName)
 
-    versionInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='alias']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    versionInput.Clear()
-    versionInput.SendKeys( asicConf.VbiosVersion )
+	versionInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='alias']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	versionInput.Clear()
+	versionInput.SendKeys(asicConf.VbiosVersion)
 
-    osInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='os']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    osInput.Clear()
-    osInput.SendKeys("Linux Ubuntu 18.04 LTS")
+	osInput, err := wd.FindElement(webdriver.ByXPATH, "//*[@id='os']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	osInput.Clear()
+	osInput.SendKeys("Linux Ubuntu 18.04 LTS")
 
-    osListBox, err := wd.FindElement(webdriver.ByXPATH, "//div[@id='mat-autocomplete-1']//span[contains(text(), 'Linux Ubuntu 18.04')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    osListBox.Click()
+	osListBox, err := wd.FindElement(webdriver.ByXPATH, "//div[@id='mat-autocomplete-1']//span[contains(text(), 'Linux Ubuntu 18.04')]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	osListBox.Click()
 
-    uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@class='submit-button mat-raised-button']")
-    if err != nil {
-        log.Fatal( err )
-    }
-    uploadBtn.Click()
+	uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[@class='submit-button mat-raised-button']")
+	if err != nil {
+		log.Fatal(err)
+	}
+	uploadBtn.Click()
 
 }
 
 func UploadBinaries(wd webdriver.WebDriver) {
 
-    if err := wd.WaitWithTimeout(uploadBtnLoaded, 10 * time.Second); err != nil {
-        log.Fatal( err )
-    }else {
-        log.Println("****** To upload binaries ******")
-    }
+	if err := wd.WaitWithTimeout(uploadBtnLoaded, 10*time.Second); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("****** To upload binaries ******")
+	}
 
-    uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'UPLOAD BINARIES')]")
-    if err != nil {
-        log.Fatal( err )
-    }
-    uploadBtn.Click()
+	uploadBtn, err := wd.FindElement(webdriver.ByXPATH, "//*[contains(text(), 'UPLOAD BINARIES')]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	uploadBtn.Click()
 
-    for index,_ := range stackConf.LnxStack {
-        uploadBIOS(wd, stackConf.LnxStack[index])
-        time.Sleep( 10 * time.Second )
-    }
+	for index, _ := range stackConf.LnxStack {
+		uploadBIOS(wd, stackConf.LnxStack[index])
+		time.Sleep(10 * time.Second)
+	}
 
-    for index,_ := range stackConf.LnxStack {
-        uploadOSDB(wd, stackConf.LnxStack[index])
-        time.Sleep( 20 * time.Second )
-    }
+	for index, _ := range stackConf.LnxStack {
+		uploadOSDB(wd, stackConf.LnxStack[index])
+		time.Sleep(20 * time.Second)
+	}
 
-    unUploadSlice := getNewBinToUpload(wd)
-    if len(unUploadSlice)>0 {
-        log.Println("There are still binaries not uplaoded!!! ====> ", unUploadSlice)
-    } else {
-        log.Println("Upload binaries successfully! Congratulations!")
-    }
+	unUploadSlice := getNewBinToUpload(wd)
+	if len(unUploadSlice) > 0 {
+		log.Println("There are still binaries not uplaoded!!! ====> ", unUploadSlice)
+	} else {
+		log.Println("Upload binaries successfully! Congratulations!")
+	}
 
 }
