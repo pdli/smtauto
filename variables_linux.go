417deef42d7aee1ceeea99312b60175fb202e224
paulil <pauline.li@amd.com>
2019-12-05 13:36:23 +0800

    split variables.go to two based on OS

---
+++ variables_linux.go
@@ -0,0 +1,58 @@
+package smtauto
+
+//    "fmt"
+
+type AsicConf struct {
+	AsicName      string
+	StackName     string
+	TargetRelease string
+	VbiosVersion  string
+	VbiosFileName string
+	OsdbVersion   string
+	OsdbID        string
+}
+
+type StackConf struct {
+	Version    string
+	StackPath  string
+	TestReport string
+	LnxStack   []AsicConf
+}
+
+type VbiosConf struct {
+	Name        string
+	Link        string
+	Version     string
+	LastVersion string
+}
+
+var (
+	targetReleaseMap = map[string]string{
+		"D182": "19.40",
+		"D187": "19.40",
+		"D188": "19.50",
+		"D189": "19.50",
+	}
+
+	asicNameMap = map[string]string{
+		"D182": "Navi10 XT",
+		"D187": "Navi10 XM",
+		"D188": "Navi10 Pro-XL",
+		"D189": "Navi10 XLE",
+	}
+
+	vbiosFileNameList = []string{
+		"D1820101",
+		"D1870101",
+		"D1880201",
+		"D1890101",
+	}
+
+	stackConf = StackConf{
+		Version:   "WW47",
+		//StackPath: "//pauline.amd.com/shares/Navi10_Stack/",
+		StackPath: "/opt/shares/Navi10_Stack/",
+	}
+
+	vbiosConf = []VbiosConf{}
+)
