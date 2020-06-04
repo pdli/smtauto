4b4ce639905672b73cf5adb89ba809b7459c2533
Paulil <pauline.li@amd.com>
2020-06-04 20:12:17 +0800

    fix typo - stackPath in conf_windows

--- conf_windows.go
+++ conf_windows.go
@@ -7,13 +7,13 @@ import (
 var (
 	stackConf = StackConf{
 		Version:   "W2021LNX",
-		StackPath: "//pauline.amd.com/shares/Navi21_Stack/",
+		StackPath: "//pauline.amd.com/shares/Navi21_Stack",
 	}
 
 	chrCaps = chrome.Capabilities{
 		Args: []string{
-			//"--headless",
-			//"--disable-gpu",
+			"--headless",
+			"--disable-gpu",
 			"--no-sandbox",
 			"--log-level=1",
 			"--window-size=1920,1080",
